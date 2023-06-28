0171b29060c686920c1ead9014d1bdc3b65bdea9
jPedroRoch4 <jpedro.devops@gmail.com>
2023-06-27 21:33:42 -0300

    2023.06.27-1 Aplicativo em modelo angular.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,69 @@
-import { NgModule } from '@angular/core';
+import { ProfileComponent } from './user/profile/profile.component';
+import { E404Component } from './pages/e404/e404.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  // Rota para a página inicial caso nao especificada
+  // ↓ Tem que ser a primeira rota
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+
+  },
+
+  // Rota para a página home.
+  {
+    path: 'home',
+    title: 'Página inicial',
+    component: HomeComponent
+  },
+
+  // Rota para a página contacts
+  {
+    path: 'contacts',
+    title: 'Faça Contato',
+    component: ContactsComponent
+  },
+
+  // Rota para a página about
+  {
+    path: 'about',
+    title: 'Sobre',
+    component: AboutComponent
+  },
+
+  // Rota para a página 404
+  {
+    path: 'e404',
+    title: 'Erro 404',
+    component: E404Component
+  },
+
+  //Rota para a página profile
+  {
+    path: 'profile',
+    title: 'Perfil de usuário',
+    component: ProfileComponent
+  },
+
+  // Rota padrão para rotas inexistentes
+  {
+    path: '**',
+    redirectTo: 'e404',
+    pathMatch: 'full'
+  }
+
+  // ↑ Tem que ser a última rota
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { ThemeComponent } from './theme/theme.component';
+import { HomeComponent } from './pages/home/home.component';
+import { E404Component } from './pages/e404/e404.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ProfileComponent } from './user/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeComponent,
+    HomeComponent,
+    E404Component,
+    AboutComponent,
+    ContactsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
